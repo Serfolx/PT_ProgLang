f9aacc41aa3ad98c18d88a195cc628327f6a1bc3
Serfolx <allenflores0100@gmail.com>
2025-03-12 12:16:35 +0800

    Calculator.py

--- index.py
+++ index.py
@@ -1,40 +1,40 @@
-# Token types
-INTEGER, PLUS, MINUS, EOF = 'INTEGER', 'PLUS', 'MINUS', 'EOF'
-class Token:
+INTEGER, PLUS, MINUS, MUL, DIV, EOF = 'INTEGER', 'PLUS', 'MINUS', 'MUL', 'DIV', 'EOF'
+
+class Token(object):
     def __init__(self, type, value):
         self.type = type
         self.value = value
     def __str__(self):
-        return f'Token({self.type}, {repr(self.value)})'
+        return 'Token({type}, {value})'.format(
+        type = self.type,
+        value = repr(self.value)
+        )
     def __repr__(self):
         return self.__str__()
-class Interpreter:
+class Lexer(object):
     def __init__(self, text):
         self.text = text
         self.pos = 0
         self.current_token = None
         self.current_char = self.text[self.pos]
     def error(self):
-        raise Exception('Invalid syntax')
+        raise Exception("Invalid character")
     def advance(self):
-        """Move the pointer to the next character in input."""
         self.pos += 1
         if self.pos > len(self.text) - 1:
-            self.current_char = None  # End of input
+            self.current_char = None
         else:
-            self.current_char = self.text[self.pos]    
+            self.current_char = self.text[self.pos]
     def skip_whitespace(self):
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
     def integer(self):
-        """Return a (multi-digit) integer from input."""
         result = ''
         while self.current_char is not None and self.current_char.isdigit():
             result += self.current_char
             self.advance()
         return int(result)
     def get_next_token(self):
-        """Tokenize the input string."""
         while self.current_char is not None:
             if self.current_char.isspace():
                 self.skip_whitespace()
@@ -46,33 +46,47 @@ class Interpreter:
                 return Token(PLUS, '+')
             if self.current_char == '-':
                 self.advance()
-                return Token(MINUS, '-')
+                return Token(MINUS, '*-')
+            if self.current_char == '*':
+                self.advance()
+                return Token(MUL, '*')
+            if self.current_char == '/':
+                self.advance()
+                return Token(DIV, '/') 
             self.error()
         return Token(EOF, None)
+        
+class Interpreter(object):
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+    def error(self):
+        raise Exception('Invalid syntax')
     def eat(self, token_type):
-        """Check token type and move to the next token if correct."""
         if self.current_token.type == token_type:
-            self.current_token = self.get_next_token()
+            self.current_token = self.lexer.get_next_token()
         else:
             self.error()
-    def term(self):
-        """Return an INTEGER token value"""
+    def factor(self):
         token = self.current_token
         self.eat(INTEGER)
         return token.value
     def expr(self):
-        """Arithmetic expression parser/interpreter.
-        Supports multiple-digit numbers and sequences of operations."""
-        self.current_token = self.get_next_token()
-        result = self.term()
-        while self.current_token.type in (PLUS, MINUS):
+        result = self.factor()
+        while self.current_token.type in (PLUS, MINUS, MUL, DIV):
             token = self.current_token
             if token.type == PLUS:
                 self.eat(PLUS)
-                result += self.term()
+                result = result + self.factor()
             elif token.type == MINUS:
                 self.eat(MINUS)
-                result -= self.term()
+                result = result - self.factor()
+            if token.type == MUL:
+                self.eat(MUL)
+                result = result * self.factor()
+            elif token.type == DIV:
+                self.eat(DIV)
+                result = result // self.factor()
         return result
 def main():
     while True:
@@ -80,10 +94,12 @@ def main():
             text = input('calc> ')
         except EOFError:
             break
-        if not text.strip():
+        if not text:
             continue
-        interpreter = Interpreter(text)
+        lexer = Lexer(text)
+        interpreter = Interpreter(lexer)
         result = interpreter.expr()
         print(result)
 if __name__ == '__main__':
     main()
+        
